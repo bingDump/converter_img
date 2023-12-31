4c9f8304b55cc2086d3f1ecc83f7fac4be6dfa5f
Abid Kamaluddin <153403603+bingDump@users.noreply.github.com>
2023-12-31 15:55:31 +0700

    Update convert_img.py
    add remove function
    
    call it to convert function

--- convert_img.py
+++ convert_img.py
@@ -16,6 +16,19 @@ def convert_images(from_ext, to_ext):
         file_image.save(item.replace(from_ext, to_ext))
         count += 1
     print(f"{count}\tfiles already coverted to .{extention_to}")
-        
+    remove_images(from_ext)   
+
+def remove_images(file_ext):
+    count =  0
+    confirmation = input("Are you sure to remove these files?  ").lower()
+    if confirmation in ['yes','ya','y']:  
+        for item in path:
+            if file_ext in item:
+                # Remove the file from the directory
+                os.remove(item)
+                count += 1
+        print(f"  {count}\tfiles removed")
+    else:
+        print("\tNo file removed")
 
 convert_images(extention_from,  extention_to)
