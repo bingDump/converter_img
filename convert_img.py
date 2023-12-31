cae00337170055a053a4dd602712402cd68b0403
Abid Kamaluddin <153403603+bingDump@users.noreply.github.com>
2023-12-31 19:46:27 +0700

    Update convert_img.py
    1. elevate converter efficiency by change the convert def to convert only image that will be convert
    2. change confirmation variable, just for readable

--- convert_img.py
+++ convert_img.py
@@ -12,15 +12,19 @@ file_convert = []
 def convert_images(from_ext, to_ext):
     count = 0
     for item in path:
-        file_image = Image.open(item).convert('RGB')
-        file_image.save(item.replace(from_ext, to_ext))
-        count += 1
+        if from_ext in item:
+            file_image = Image.open(item).convert('RGB')
+            file_image.save(item.replace(from_ext, to_ext))
+            count += 1
+        else:
+            continue
+    
     print(f"{count}\tfiles already coverted to .{extention_to}")
-    remove_images(from_ext)   
+    remove_images(from_ext) 
 
 def remove_images(file_ext):
     count =  0
-    confirmation = input("Are you sure to remove these files?  ").lower()
+    confirmation = input(f"Confirm .{file_ext} will be removed?  ").lower()
     if confirmation in ['yes','ya','y']:  
         for item in path:
             if file_ext in item:
